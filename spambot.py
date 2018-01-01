c47045d14d715732e7446c7251e185938281ba4c
cheddar-cheeze <hacksteam55@gmail.com>
2018-01-01 02:22:51 +0000

    added feature to invite bot to target server

--- spambot.py
+++ spambot.py
@@ -10,6 +10,7 @@ token = ("enter ur token here")
 client = discord.Client()
 raidtxt = str(input("plz enter in spam text:"))
 spamamt = int(input("plz enter the amt of spam u want:"))
+invitelink = str(input("plz enter an invite link for server ud like to raid: "))
 
 @client.event
 async def on_ready():
@@ -21,5 +22,9 @@ async def on_message(message):
         for x in range(0, spamamt):
             time.sleep(1)
             await client.send_message(message.channel, raidtxt)
+
+@client.event
+async def on_ready():
+    await client.accept_invite(invitelink)
 
 client.run(token, bot=False)
